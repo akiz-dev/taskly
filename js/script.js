4690e5d92a29dcb3d8b5b9109ae555be04aea93b
akiz <akiz.dev@gmail.com>
2026-03-10 18:17:04 -0300

    create hero section

--- js/script.js
+++ js/script.js
@@ -6,16 +6,14 @@ var body = document.querySelector('body');
 // show navbar and hide menu button mobile and show close menu button
 $('#btnMenuMobile').on('click', ()=>{
     $('#navbarHeader').toggle('slow')
-    body.style.background = '#1E293B80'
 
     btnMenuMobile.style.display = 'none';
     btnMenuClose.style.display = 'block';
-    btnMenuClose.style.color = '#F8FAFC';
+    btnMenuClose.style.color = '#0F172A';
 })
 
 $('#btnMenuClose').on('click', ()=> {
     $('#navbarHeader').toggle('slow')
-    body.style.background = '#F8FAFC'
 
     btnMenuClose.style.display = 'none';
     btnMenuMobile.style.display = 'block'

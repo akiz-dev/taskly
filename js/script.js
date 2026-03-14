7ec96c656de0a0a74b02f737ba6750fdad6d9c8b
akiz <akiz.dev@gmail.com>
2026-03-14 19:10:06 -0300

    navbar header finalizado

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
-var btnMenuMobile = document.getElementById('btnMenuMobile');
-var btnMenuClose = document.getElementById('btnMenuClose');
+let btnMenuMobile = document.getElementById('btnMenuMobile');
+let btnMenuClose = document.getElementById('btnMenuClose');
 
-var body = document.querySelector('body');
+let header = document.getElementById('header');
 
-// show navbar and hide menu button mobile and show close menu button
-$('#btnMenuMobile').on('click', ()=>{
-    $('#navbarHeader').toggle('slow')
 
-    btnMenuMobile.style.display = 'none';
-    btnMenuClose.style.display = 'block';
-    btnMenuClose.style.color = '#0F172A';
+// adiciona e remove cor de fundo do elemento ehader quando ativa o scroll da pagina
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 10) {
+        header.classList.add("headerScrolled")
+    } else {
+        header.classList.remove("headerScrolled")
+    }
+})
+// ***
+
+
+// mostra menu e troca o icone do botão e cor do background do header
+btnMenuMobile.addEventListener('click', ()=> {
+    $(btnMenuClose).slideDown();
+    $(btnMenuMobile).hide();
+
+    // navbarHeader show
+    $('#navbarHeader').slideToggle();
+
+    header.classList.add('headerBgColor');
+    
 })
 
-$('#btnMenuClose').on('click', ()=> {
-    $('#navbarHeader').toggle('slow')
 
-    btnMenuClose.style.display = 'none';
-    btnMenuMobile.style.display = 'block'
+btnMenuClose.addEventListener('click', ()=>{
+    $(btnMenuClose).hide();
+    $(btnMenuMobile).slideDown();
 
+    // navbarHeader hide
+    $('#navbarHeader').slideToggle();
+
+    header.classList.remove('headerBgColor')
 })
